3e5970c0a4c9584f8680f7c1f8c00abb76cff68e
tuuzdu <tuuzdu@mail.ru>
2018-05-07 17:32:55 +0300

    ipfs api revision

--- de_airsense_ros/src/de_airsense_waspmote_ipfs.py
+++ de_airsense_ros/src/de_airsense_waspmote_ipfs.py
@@ -12,15 +12,26 @@ import rospy
 
 DEFAULT_PORT = '/dev/ttyUSB0'
 BAUDRATE = 115200
+STOP_SYMBOL = b'$'
 RATE_HZ = 1
 IN_AIR_STANDBY = 3
 ON_GROUND = 1
 
 status_in_air = False
 status_to_send = False
-altitude = 0
+altitude = 0.0
+latitude = 0.0
+longitude = 0.0
 
-def write_send_data(frame_array):
+def ipfs_send (api, file):
+    # for i in range(3):
+    try:
+        res = api.add(file)
+        return res
+    except:
+        return 'IPFS error'
+
+def write_send_data(frame_array, api_loc, api_rem):
     fileName = 'data_'
     dir = os.path.dirname(__file__)
     folderName = os.path.join(dir, 'sensor_data/')
@@ -36,19 +47,16 @@ def write_send_data(frame_array):
     with open (path, 'w') as f:
         for string in frame_array:
             f.write(string)
+    print ('File wrote in')
+    print (path)
 
-    api = ipfsapi.connect('127.0.0.1', 5001)
-    # api = ipfsapi.connect('52.178.98.62', 9095)
-    res = api.add(path)
+    print ('IPFS remote node response:')
+    res = ipfs_send(api_rem, path)
     print (res)
-    # time.sleep(1)
-    if res != None:
-        path = folderName + fileName + timestr + '_' + res['Hash'] + '.txt'
-        with open (path, 'w') as f:
-            for string in frame_array:
-                f.write(string)
-    #     os.rename(fileName, fileName[:-4] + res['Hash'] + fileName[-4:])
-
+    print ('IPFS local node response:')
+    res = ipfs_send(api_loc, path)
+    print (res)
+    
 def status_cb(data): 
     global status_in_air
     global status_to_send
@@ -57,13 +65,16 @@ def status_cb(data):
         print 'Start to write data'
     elif data.data == ON_GROUND and status_in_air == True:
         print 'Stop to write data'
-        # write_send_data()
         status_to_send = True
         status_in_air = False
 
 def position_cb(data):
     global altitude
+    global latitude
+    global longitude
     altitude = data.altitude
+    latitude = data.latitude
+    longitude = data.longitude
 
 if __name__ == '__main__':
     print 'Starting waspmote gas sensors...'
@@ -71,6 +82,8 @@ if __name__ == '__main__':
     rospy.init_node('de_airsense_waspmore_ipfs')
     rospy.Subscriber('dji_sdk/flight_status', UInt8, status_cb)
     rospy.Subscriber('dji_sdk/gps_position', NavSatFix, position_cb)
+    ipfs_api_loc = ipfsapi.connect('127.0.0.1', 5001)
+    ipfs_api_rem = ipfsapi.connect('52.178.98.62', 9095)
 
     rate = rospy.Rate(RATE_HZ)
     frame = ''
@@ -87,9 +100,12 @@ if __name__ == '__main__':
                     print 'Waspmote gas sensors and ROS are ready'
                 byte = serial_port.read()
 
-                if byte == b'\n':
-                    frame += byte.decode()  
-                    frame += 'ALT:{0:.1f}#{1:s}'.format(altitude, time.strftime('%Y/%m/%d %H:%M:%S'))
+                if byte == STOP_SYMBOL:
+                    # frame += byte.decode()  
+                    frame += 'Copter Latitude: {0:.6f}\nCopter Longitude: {1:.6f}\nCopter Altitude: {2:.2f} m\nSystem time: {3:s}\n'.format(   latitude, 
+                                                                                                                        longitude, 
+                                                                                                                        altitude, 
+                                                                                                                        time.strftime('%Y/%m/%d %H:%M:%S'))
                     frame_array.append(frame)
                     print (frame)
                     frame = ''
@@ -99,7 +115,7 @@ if __name__ == '__main__':
 
             if status_to_send:
                 status_to_send = False
-                write_send_data(frame_array)
+                write_send_data(frame_array, ipfs_api_loc, ipfs_api_rem)
                 frame_array = []
             elif status_in_air == False:
                 frame_array = []
